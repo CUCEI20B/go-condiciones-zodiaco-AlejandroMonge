2791b6ee7b6ca90215958f6bd90f1f30876efe23
AlejandroMonge <kmonge_rey@hotmail.com>
2020-09-30 14:21:06 -0500

    Prueba01

--- main.go
+++ main.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var dia int32
+	var mes int32
+
+	fmt.Scanln(&dia)
+	fmt.Scanln(&mes)
+
+	if (dia >= 21 && mes == 3) || (dia <= 20 && mes == 4) {
+		fmt.Println("aries")
+	} else if (dia >= 24 && mes == 9) || (dia <= 23 && mes == 10) {
+		fmt.Println("libra")
+	} else if (dia >= 21 && mes == 4) || (dia <= 21 && mes == 5) {
+		fmt.Println("tauro")
+	} else if (dia >= 24 && mes == 10) || (dia <= 22 && mes == 11) {
+		fmt.Println("escorpio")
+	} else if (dia >= 22 && mes == 5) || (dia <= 21 && mes == 6) {
+		fmt.Println("geminis")
+	} else if (dia >= 23 && mes == 11) || (dia <= 21 && mes == 12) {
+		fmt.Println("sagitario")
+	} else if (dia >= 21 && mes == 6) || (dia <= 23 && mes == 7) {
+		fmt.Println("cancer")
+	} else if (dia >= 22 && mes == 12) || (dia <= 20 && mes == 1) {
+		fmt.Println("capricornio")
+	} else if (dia >= 24 && mes == 7) || (dia <= 23 && mes == 8) {
+		fmt.Println("leo")
+	} else if (dia >= 21 && mes == 1) || (dia <= 19 && mes == 2) {
+		fmt.Println("acuario")
+	} else if (dia >= 24 && mes == 8) || (dia <= 23 && mes == 9) {
+		fmt.Println("virgo")
+	} else if (dia >= 20 && mes == 2) || (dia <= 20 && mes == 3) {
+		fmt.Println("piscis")
+	}
+
 }
